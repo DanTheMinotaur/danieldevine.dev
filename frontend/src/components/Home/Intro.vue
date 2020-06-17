0a7b5c5205d333e09daad1c1fe70f91199a81a40
Daniel <daniel.devine1@outlook.com>
2020-06-17 19:59:03 +0100

    Add homepage gql query

--- frontend/src/components/Home/Intro.vue
+++ frontend/src/components/Home/Intro.vue
@@ -1,13 +1,14 @@
 <template>
   <!-- Me -->
   <article id="home" class="panel intro">
+    
     <header>
-      <h1>Jane Doe</h1>
-      <p>Senior Astral Projectionist</p>
+      <h1>{{homePage.header}}</h1>
+      <p>{{homePage.sub_header}}</p>
     </header>
     <a href="#work" class="jumplink pic">
       <span class="arrow icon solid fa-chevron-right"><span>See my work</span></span>
-      <img src="images/me.jpg" alt="" />
+      <img :src="getURL(homePage.profile_image.url)" :alt="homePage.alternativeText" :title="homePage.name" />
     </a>
   </article>
             
@@ -23,23 +24,20 @@ export default {
   components: {},
   data() {
     return {
-      homePage: {},
-      currentSection: 'banner'
+      homePage: {}
     }
   },
   apollo: {
     homePage: gql`
       query {
         homePage {
-          header,
-          sub_header,
-          header_image {
-            name,
-            url,
+          header
+          sub_header
+          profile_image {
+            url
+            name
             alternativeText
-          },
-          about,
-          about_header
+          }
         }
       }
     `
