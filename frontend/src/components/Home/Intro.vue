8cbb6808eb7bfcade3020abd276864eff6522727
Daniel <daniel.devine1@outlook.com>
2020-06-17 22:19:05 +0100

    Reformat

--- frontend/src/components/Home/Intro.vue
+++ frontend/src/components/Home/Intro.vue
@@ -1,36 +1,35 @@
 <template>
-  <!-- Me -->
   <article id="home" class="panel intro">
-    
     <header>
       <h1>{{homePage.header}}</h1>
       <p>{{homePage.sub_header}}</p>
     </header>
     <span class="jumplink pic">
-      <router-link 
-        :to="{path: 'projects'}"
-        class="arrow icon solid fa-chevron-right"><span>See my work</span></router-link>
-      <img :src="getURL(homePage.profile_image.url)" :alt="homePage.alternativeText" :title="homePage.name" />
+      <router-link :to="{path: 'projects'}" class="arrow icon solid fa-chevron-right">
+        <span>See my work</span>
+      </router-link>
+      <img
+        :src="getURL(homePage.profile_image.url)"
+        :alt="homePage.alternativeText"
+        :title="homePage.name"
+      />
     </span>
   </article>
-            
 </template>
 
 <script>
+import gql from "graphql-tag";
 
-
-import gql from "graphql-tag"
-
-export default {  
-  name: 'Intro',
+export default {
+  name: "Intro",
   components: {},
   data() {
     return {
       homePage: {}
-    }
+    };
   },
   apollo: {
-    homePage: gql`
+    homePage: gql(`
       query {
         homePage {
           header
@@ -42,10 +41,9 @@ export default {
           }
         }
       }
-    `
+    `)
   }
-}
-
+};
 </script>
 
 
