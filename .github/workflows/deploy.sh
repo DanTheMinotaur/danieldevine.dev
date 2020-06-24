15e9406d4a49cd27fa4ec25ffeecca27e5d85150
Daniel <daniel.devine1@outlook.com>
2020-06-24 19:17:17 +0100

    Deploy for front end

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
-cd ${$1}/frontend
+#!/bin/sh
+echo "Building for $1 environment"
+cd ${$2}/frontend
 
-echo "VUE_APP_GRAPHQL_URL:$2\n" >> .env
+echo -e "VUE_APP_GRAPHQL_URL:$3" >> .env
+echo -e "VUE_APP_API_URL:$4" >> .env
+echo -e "VIRTUAL_HOST:$5" >> .env
+echo -e "VIRTUAL_PORT:$6" >> .env
+echo -e "LETSENCRYPT_HOST:$7" >> .env
+
+docker-compose down
+docker-compose up --build -d
