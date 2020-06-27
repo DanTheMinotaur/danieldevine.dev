7f8a01da6d554276c03c1ca2d6f556dfa1c44614
Daniel <daniel.devine1@outlook.com>
2020-06-27 15:47:20 +0200

    Fix vars

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -2,8 +2,8 @@
 git checkout $1
 git pull
 echo "Building for $1 front end environment"
-cd ${$2}/frontend && docker-compose down && docker-compose up --build -d
+cd $2/frontend && docker-compose down && docker-compose up --build -d
 
 echo "Building for $1 back end environment"
-cd ${$2}/backend && docker-compose down && docker-compose up --build -d
+cd $2/backend && docker-compose down && docker-compose up --build -d
 
