11cedba197eb783171f58c267f67c491a66f8eef
dantheminotaur <daniel.devine1@outlook.com>
2020-07-01 00:31:10 +0100

    Update deploy

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -2,9 +2,10 @@
 
 cd $2
 
+git pull
+
 git checkout $1
 
-git pull
 
 echo "Building for $1 front end environment"
 cd $2/frontend && docker-compose down && docker-compose up --build -d
--- frontend/src/components/Home/Intro.vue
+++ frontend/src/components/Home/Intro.vue
@@ -1,33 +1,65 @@
 <template>
-  <article id="home" class="panel intro">
-    <header>
-      <h1>{{homePage.header}}</h1>
-      <p>{{homePage.sub_header}}</p>
-    </header>
-    <span class="jumplink pic">
-      <router-link :to="{path: 'projects'}" class="arrow icon solid fa-chevron-right">
-        <span>See my work</span>
-      </router-link>
-      <img
-        :src="getURL(homePage.profile_image.url)"
-        :alt="homePage.alternativeText"
-        :title="homePage.name"
-        v-if="homePage.profile_image"
-      />
-    </span>
-  </article>
+  <div>
+    <article id="home" class="panel intro">
+      <header>
+        <h1>{{homePage.header}}</h1>
+        <p>{{homePage.sub_header}}</p>
+      </header>
+      <span class="jumplink pic">
+        <router-link :to="{path: 'projects'}" class="arrow icon solid fa-chevron-right">
+          <span>See my work</span>
+        </router-link>
+        <img
+          :src="getURL(homePage.profile_image.url)"
+          :alt="homePage.alternativeText"
+          :title="homePage.name"
+          v-if="homePage.profile_image"
+        />
+      </span>
+    </article>
+    <aside>
+      <h2>Things I can do</h2>
+      <section>
+        <p>Here is some content</p>
+        <TechnologiesBar :icons="testD"/>
+      </section>
+    </aside>
+  </div>
 </template>
 
 <script>
 import gql from "graphql-tag";
+import TechnologiesBar from './Technologies.vue'
 
 export default {
   name: "Intro",
-  components: {},
+  components: {
+    TechnologiesBar
+  },
   data() {
     return {
-      homePage: {}
-    };
+      homePage: {},
+      testD: [
+          {
+            "name": "Google Firebase",
+            "image_icon": {
+              "url": "/uploads/logo_firebase_icon_cae9049b5a.svg"
+            }
+          },
+          {
+            "name": "JavaScript",
+            "image_icon": {
+              "url": "/uploads/kissclipart-js-icon-png-clipart-javascript-computer-icons-aac591a39aab9a64_d35e735f69.png"
+            }
+          },
+          {
+            "name": "Python",
+            "image_icon": {
+              "url": "/uploads/python-logo_37d479eb41.png"
+            }
+          }
+        ]
+    }
   },
   apollo: {
     homePage: gql`
