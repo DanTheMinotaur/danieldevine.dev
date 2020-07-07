8d4eedb6f810df9d069aad1b6d7ba46af5207767
dantheminotaur <daniel.devine1@outlook.com>
2020-07-07 18:33:32 +0100

    Updates to deploy

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -11,5 +11,5 @@ echo "Building for $1 front end environment"
 cd $2/frontend && docker-compose down && docker-compose up --build -d
 
 echo "Building for $1 back end environment"
-cd $2/backend && docker-compose down && docker-compose up --build -d
+cd $2/backend && docker-compose down && docker-compose up --build -d -f docker-compose-$1.yml
 
