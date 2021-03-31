84a443490d1b978a698e9552c25bbee7b51ecad9
daniel <me@danieldevine.dev>
2021-03-31 22:26:46 +0100

    Disable workflows

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -16,3 +16,4 @@ cd $2/backend && docker-compose -f docker-compose-$1.yml up --build -d
 docker build --build-arg NODE_ENV='staging' --build-arg PORT=1337  .
 
 docker build --build-arg NODE_ENV='staging' --build-arg PORT=1337 --tag danieldevine.dev/backend:staging .
+
