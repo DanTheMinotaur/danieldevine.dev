fc481c442269c74b0769e323aa80edb5f6d021f7
Dan <daniel@fail.ie>
2023-10-26 12:16:34 +0100

    Update dev configs

--- backend/config/env/development/database.js
+++ backend/config/env/development/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: env('DATABASE_CLIENT', 'sqlite3'),
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        filename: env('DATABASE_FILENAME', '.tmp/db.sqlite'),
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'strapi'),
